2493b142b461b77a32465502b04e58f9f68d4956
Elise G <55546805+EliseGallois@users.noreply.github.com>
2026-02-24 15:51:02 +0000

    Add files via upload

---
+++ inst/app/www/gtag.js
@@ -0,0 +1,8 @@
+<script async src="ANONYMISED"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ANONYMISED');
+</script>
